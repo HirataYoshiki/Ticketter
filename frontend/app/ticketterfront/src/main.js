a189c440d11ab9df6cadb7fbb12dd5cf7c017a9e
Italy25 <kirryx2@gmail.com>
2021-05-25 15:50:33 +0900

    no message

--- frontend/app/ticketterfront/src/main.js
+++ frontend/app/ticketterfront/src/main.js
@@ -2,16 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import firebase from 'firebase'
+import config from '../.env/config'
 
 Vue.config.productionTip = false
-var firebaseConfig = {
-  apiKey: "AIzaSyDcHeQbYaKJQ8cTmyEDDH1P314X8JruIck",
-  authDomain: "ticketter.firebaseapp.com",
-  projectId: "ticketter",
-  storageBucket: "ticketter.appspot.com",
-  messagingSenderId: "941864709396",
-  appId: "1:941864709396:web:d252f2c8acad7475ca6601"
-}
+var firebaseConfig = config
 // Initialize Firebase 
 firebase.initializeApp(firebaseConfig)
 
